012f5ff146d88239b93288d8cc0b729026ed1225
Nellie McKesson <nellie@oreilly.com>
2013-08-22 14:56:42 -0400

    removing old widget route from app

--- site/app.rb
+++ site/app.rb
@@ -25,13 +25,3 @@ get '/chapters/:q' do
     @markdown = @bodytext.gsub(/https:\/\/github.com\/atduskgreg\/opencv-processing-book\/blob\/master\/book\/[a-z]*\//, "/chapters/").gsub(/\.md/,"").gsub(/<!--START WIDGET-->/, "</div></div><div id=\"myCarousel\" class=\"carousel slide\" data-interval=\"false\"><div class=\"carousel-inner\"><div class=\"active item\"><div class=\"row-fluid\"><div class=\"block span4\">").gsub(/<!--SLIDEBREAK-->/, "</div></div></div><div class=\"item\"><div class=\"row-fluid\"><div class=\"block span4\">").gsub(/<!--COLBREAK-->/, "</div><div class=\"block span4\">").gsub(/(<div class="block span4">)(\n)/, "\\1").gsub(/(\[)(.*)(\])(\()(#.*)(\))({id=)(.*)(})/, "<a href=\"\\5\" class=\"callout\" id=\"\\8\">\\2<\/a>").gsub(/(<a)( href=".*">.*<\/a>)({id=)(.*)(})/,"\\1 class=\"callout\" id=\"\\4\"\\2").gsub(/<!--END WIDGET-->/, "</div></div></div><a class=\"carousel-control left\" href=\"#myCarousel\" data-slide=\"prev\">&lsaquo;</a><a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">&rsaquo;</a></div><div id=\"container\" class=\"row-fluid\"><div class=\"span8 offset2\">")
     erb :chapter
 end
-
-get '/widgets/:q' do
-	@title = "#{params[:q]}.pde"
-	@files = Dir.glob("#{params[:q]}*.md")
-	first_file = @files.shift
-	@widgetfirstmd = File.read(first_file)
-	@widgetfirsthtml = BlueCloth.new(@widgetfirstmd).to_html
-	@widgetfirst = @widgetfirsthtml.gsub(/<!--START BLOCK-->/, "<div class=\"block span4\">").gsub(/<!--END BLOCK-->/, "</div>").gsub(/(<div class="block span4">)(\n)/, "\\1").gsub(/(\[)(.*)(\])(\()(#.*)(\))({id=)(.*)(})/, "<a href=\"\\5\" class=\"callout\" id=\"\\8\">\\2<\/a>").gsub(/(<a)( href=".*">.*<\/a>)({id=)(.*)(})/,"\\1 class=\"callout\" id=\"\\4\"\\2")
-	erb :widget
-end
